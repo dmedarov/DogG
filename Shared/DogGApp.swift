aba362449c82aa7138f7ab9003ee674a294efc22
Damian Medarov <d.medarov@me.com>
2021-01-29 22:08:00 +0200

    Initial1

---
+++ Shared/DogGApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGApp.swift
+//  Shared
+//
+//  Created by Damian Medarov on 29.01.21.
+//
+
+import SwiftUI
+
+@main
+struct DogGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
